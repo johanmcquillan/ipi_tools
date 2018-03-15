Fixed unit conversion issues

python/packages/potentials.py
@@ -11,17 +11,17 @@ kJ2eV = 6.241509125E21
 
 class PotentialEnergySurface(object):
     __metaclass__ = ABCMeta
-
+    
     def __init__(self, w, c):
         self.w = w / bohr2nm
         self.update_cell(c)
-
+    
     def update_cell(self, c):
-        assert self.w < c
-        self.c = c
-        self.z0 = (c - self.w)/2
-        self.z1 = (c + self.w)/2
-
+        assert self.w < c / bohr2nm
+        self.c = c / bohr2nm
+        self.z0 = (self.c - self.w)/2
+        self.z1 = (self.c + self.w)/2
+    
     def pbc(self, z):
         while np.any(z < 0):
             z += self.c * (z < 0).astype(int)
@@ -32,11 +32,11 @@ class PotentialEnergySurface(object):
     @abstractmethod
     def potential(self, r):
         pass
-
+    
     @abstractmethod
     def gradient(self, r):
         pass
-
+    
     def force(self, r):
         return -self.gradient(r)
 
@@ -48,7 +48,7 @@ class Morse1D(PotentialEnergySurface):
         self.a = 0.92 * bohr2angs
         self.D = 57.8E-3 * ev2har
         super(Morse1D, self).__init__(w, c)
-
+    
     def potential(self, z):
         if np.any(z <= self.z0) or np.any(z >= self.z1):
             n0 = np.sum((z <= self.z0).astype(int))
@@ -57,8 +57,8 @@ class Morse1D(PotentialEnergySurface):
         V = np.zeros(z.shape)
         V = self.D * ((1.0 - np.exp(-self.a*(z - self.z0 - self.zeta)))**2 +
                     (1.0 - np.exp(-self.a*(self.z1 - z - self.zeta)))**2 - 2.0)
-        return np.sum(V)
-
+        return V
+    
     def gradient(self, z):
         F = np.zeros(z.shape)
         F = 2 * self.a * self.D * (np.exp(-self.a*(z - self.z0 - self.zeta)) - np.exp(-2*self.a*(z - self.z0 - self.zeta)) - 
@@ -74,7 +74,7 @@ class LennardJones1D(PotentialEnergySurface):
         self.epsilon = 2.092 / L * kJ2eV * ev2har # 2.092 kJ/mol, converted to Ha
         self.w = w / bohr2nm
         super(LennardJones1D, self).__init__(w, c)
-
+    
     def potential(self, z):
         if np.any(z <= self.z0) or np.any(z >= self.z1):
             n0 = np.sum((z <= self.z0).astype(int))
@@ -84,7 +84,7 @@ class LennardJones1D(PotentialEnergySurface):
         V = self.epsilon *  (self.factor*(self.sigma/(z - self.z0))**9 + (self.sigma/(z - self.z0))**3 + 
                              self.factor*(self.sigma/(self.z1 - z))**9 + (self.sigma/(self.z1 - z))**3)
         return V
-
+    
     def gradient(self, z):
         F = np.zeros(z.shape)
         F = self.epsilon *  (-self.factor*9.*self.sigma**9/(z - self.z0)**10 - 3.*self.sigma**3/(z - self.z0)**4 + 